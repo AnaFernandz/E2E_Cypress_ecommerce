7cc58fc122dbf24b603e80db5d5e235c17e9cca2
Ana Fernandez <apbfmm@gmail.com>
2023-03-20 17:38:24 +0100

    E2E Cypress

---
+++ cypress/e2e/e2e.cy.js
@@ -0,0 +1,48 @@
+/// <reference types= "cypress" />
+
+const perfil = require('../fixtures/perfil.json')
+import CheckoutPage from '../support/page_objects/checkout.page'
+import checkout from '../fixtures/checkout.json'
+
+context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
+    /*  Como cliente 
+        Quero acessar a Loja EBAC 
+        Para fazer um pedido de 4 produtos 
+        Fazendo a escolha dos produtos
+        Adicionando ao carrinho
+        Preenchendo todas opções no checkout
+        E validando minha compra ao final */
+
+    beforeEach(() => {
+        cy.visit('minha-conta')
+        cy.fixture('perfil').then(dados => {
+            cy.login(dados.usuario, dados.senha)
+        })
+    })
+
+    it('Deve selecionar um produto da lista - usando comando customizado', () => {
+        cy.visit('produtos')
+        cy.addProdutos('Arcadio Gym Short', '34', 'Red', 3)
+
+        CheckoutPage.dadosCheckout(
+            checkout[1].nome,
+            checkout[1].sobrenome,
+            checkout[1].empresa,
+            checkout[1].pais,
+            checkout[1].endereco,
+            checkout[1].cidade,
+            checkout[1].estado,
+            checkout[1].cep,
+            checkout[1].telefone,
+            checkout[1].email
+        )
+    });
+
+
+});
+
+
+
+
+
+
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,3 +31,16 @@ Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('.woocommerce-form > .button').click()
 });
 
+Cypress.Commands.add('addProdutos', (produto, tamanho, cor, quantidade) =>{
+
+    cy.get('[class= "product-block grid"]')
+    .contains(produto).click()
+    cy.get('.button-variable-item-' + tamanho).click()
+    cy.get('.button-variable-item-' + cor ).click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
+    cy.get('#cart > .dropdown-toggle').click()
+    cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
+
+  })
+
